be12a5beeb5eed11a8be02c36c62977a3ae26cd8
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2024-07-06 19:37:23 +0545

    chaninging in helpers

---
+++ frontend/src/helpers/displayCurrency.js
@@ -0,0 +1,12 @@
+const displayNPRCurrency = (num) => {
+    const formatter = new Intl.NumberFormat('en-IN', {
+        style: "currency",
+        currency: 'NPR',
+        minimumFractionDigits: 2
+    })
+
+    return formatter.format(num)
+
+}
+
+export default displayNPRCurrency
---
+++ frontend/src/helpers/productCategory.js
@@ -0,0 +1,13 @@
+const productCategory = [
+    { id: 1, label: "Full Helmet", value: "Full Helmet" },
+    { id: 2, label: "Half Helmet", value: "Half Helmet" },
+    { id: 3, label: "Dirt Helmet", value: "Dirt Helmet" },
+    { id: 4, label: "Motor Oil", value: "Motor Oil" },
+    { id: 5, label: "Gloves", value: "Gloves" },
+    { id: 6, label: "Tyres", value: "Tyres" },
+    { id: 7, label: "Raincoat", value: "Raincoat" },
+    { id: 8, label: "Goggles", value: "Goggles" },
+]
+
+
+export default productCategory
